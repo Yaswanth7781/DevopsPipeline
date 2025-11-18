8d5ad64d030574da29e3de84a94cffd8434b37ef
Yaswanth7781 <padamyaswanth@gmail.com>
2025-11-18 08:43:16 +0000

    added statement in java App

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -9,5 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Devops Java Pipeline Running successful" );
+	System.out.println("hi yaswanth");
     }
 }
