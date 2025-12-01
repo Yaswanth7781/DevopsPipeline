4392b5f5c5cff7db404890db9987e9c5f4955057
Yaswanth7781 <padamyaswanth@gmail.com>
2025-12-01 18:54:38 +0000

    updated App.java

--- src/main/java/com/devops/app/App.java
+++ src/main/java/com/devops/app/App.java
@@ -9,10 +9,5 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Devops Java Pipeline Running successful" );
-	System.out.println("hi yaswanth");
-   	System.out.println("how are you");
-	int a=10;
-	int b=20;
-	System.out.println(a+b);
     }
 }
